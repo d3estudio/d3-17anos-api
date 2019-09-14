ec20535ef7945f64e58e5bf58f145f57cb736874
Matheus Portillo <mathportillo@gmail.com>
2019-09-14 11:02:13 -0300

    feat: time with

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -16,9 +16,24 @@ class Guest < ApplicationRecord
     self.slug
   end
 
+  def time_with
+    seconds_per_month = 60 * 60 * 24 * 30
+    months = (self.joined_at - DateTime.now) / seconds_per_month
+    months = months.abs.floor
+    if months < 12 then
+      return "#{months} MESES" if months > 1
+      return "#{months} MES"
+    end
+
+    years = months / 12
+
+    return "#{years} ANOS" if years > 1
+    return "#{years} ANO"
+  end
+
   private
 
   def set_defaults
-    self.tag_nfc = self.tag_nfc.gsub(":", '').upcase() if self.tag_nfc.present?
+  self.tag_nfc = self.tag_nfc.gsub(":", '').upcase() if self.tag_nfc.present?
   end
 end
--- app/views/guests/_guest.json.jbuilder
+++ app/views/guests/_guest.json.jbuilder
@@ -1,3 +1,3 @@
 json.extract! guest, :id, :name, :nickname, :tag_nfc, :description,
-  :greeting, :bond, :phone, :address, :address_number,
+  :greeting, :bond, :phone, :address, :address_number, :time_with,
   :address_complement, :zip, :city, :state, :slug, :picture, :email
