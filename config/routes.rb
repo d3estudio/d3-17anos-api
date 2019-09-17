98f68feacff024ccf1866bd4515b2be2755c73b8
Matheus Portillo <mathportillo@gmail.com>
2019-09-17 11:45:31 -0300

    disable gate and cable

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  mount ActionCable.server => '/cable'
+  # mount ActionCable.server => '/cable'
 
   defaults format: :json do
     resources :guests, only: [:index]
     resources :checkins, only: [:index] do
-      get 'create/:slug', action: :create, on: :collection
+      # get 'create/:slug', action: :create, on: :collection
     end
   end
 end
